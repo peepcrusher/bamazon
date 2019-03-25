79eedba11275b686e6b1631b083655d59d99aa77
Sam Thompson <electronic62794@yahoo.com>
2019-03-25 09:59:05 -0700

    started inquirer prompts

--- bamazon.js
+++ bamazon.js
@@ -1,2 +1,24 @@
 //require inquirer 
 var inquirer = require("inquirer");
+
+//start up the store
+function start() {
+    console.log("Welcome to our shop!")
+    menu();
+}
+
+function menu(){
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Buy", "List new item", "Exit"],
+            name: "action"
+        }
+    ])
+
+    switch(userInput){
+        case "Buy": 
+        buyItem();
+    }
+}
